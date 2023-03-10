e51ac3ea22e21ec24f768a0f2dc1809c596cb9fc
clchua1 <chualiang97@gmail.com>
2023-03-10 19:46:02 +0800

    update project
    update the information to be display in app

--- main.py
+++ main.py
@@ -163,16 +163,7 @@ with tabs[0]:
 
 with tabs[1]:
     
-    st.info('**Potential Analysis:**\
-            Business could analyze customer feedback to identify common themes\
-            and issues with their products. They could use this information to\
-            improve product quality, identify potential product features, and \
-            adjust pricing strategies based on customer satisfaction. Additionally,\
-            businesses could compare customer reviews levels across different \
-            products and use this information to prioritize resources and \
-            investments in different products lines. By identifying common themes\
-            in customer feedback, businesses could take targeted action to improve\
-            product quality and overall customer satisfaction.')
+    Info().tab1_info()
     
     options = st.selectbox('Choose to upload dataset or preview using sample dataset',
                            ['upload dataset','sample dataset'])
@@ -202,66 +193,26 @@ with tabs[1]:
 
 with tabs[2]:
     
-    st.info('**Potential Analysis:**\
-            Businesses or organizations could analyze public sentiment about their\
-            brand, product, or topic on social media to identify potential issues\
-            or opportunities for engagement. They could use this information to \
-            adjust their marketing or communication strategies, respond to customer\
-            feedback, or address issues in real-time. Additionally, businesses could\
-            track changes in public sentiment over time and compare sentiment levels\
-            across different topics or brands to gain insights into broader trends\
-            or patterns in public opinion. By analyzing public sentiment on social\
-            media, businesses can better understand customer perceptions and take\
-            targeted action to improve brand reputation and customer satisfaction.')
-
+    Info().tab2_info()
 
 #%% Tab 4: Political 
 
 with tabs[3]:
     
-    st.info('**Potential Analysis**\
-            Political campaigns or organizations could analyze public sentiment\
-            about political candidates or issues on social media to identify\
-            potential issues or opportunities for engagement. They could use\
-            this information to adjust their messaging, respond to public feedback\
-            or address concerns in real-time. Additionally, they could track\
-            changes in public sentiment over time and compare sentiment levels\
-            accross different candidates or issues to gain insights into broader\
-            trends or patterns in public opinion. By analyzing public sentiment\
-            on social media, political campaigns or organization can better \
-            understand public perceptions and take targeted action to improve\
-            their chances of success.')
+    Info().tab3_info()
 
 #%% Tab 5: Employee 
 
 with tabs[4]:
     
-    st.info('**Potential Analysis:**\
-            Companies could analyze employee feedback to identify common themes\
-            and issues related to employee satisfaction, such as management\
-            style, work-life balance, or compensation. They could use this\
-            information to improve company policies, address employee concerns,\
-            and enhance overall employee satisfaction. Additionally, companies\
-            could compare employee satisfaction across different departments\
-            or locations and use this information to prioritize resources and\
-            investments in different areas of the organization. By identifying\
-            common themes in employee feedback, companies can take targeted action\
-            to improve employee satisfaction, reduce employee turnover, and create\
-            a positive workplace culture.')
+    Info().tab4_info()
 
 
 #%% Tab 6: Healthcare
 
 with tabs[5]:
     
-    st.info('**Potential Analysis:**\
-            Healthcare providers could analyze patient feedback to identify areas\
-            for improvement, such as communication with patients, wait times, or\
-            overall experience. They could also track changes in patient\
-            satisfaction over time and compare satisfaction levels accross\
-            different hospitals. By analyzing common themes in patient feedback,\
-            healthcare providers could take targeted action to improve patient\
-            experience and overall satisfaction.')
+    Info().tab5_info()
 
 
 
